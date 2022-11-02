0830f687619027612c5dacd2de0c0c8b30c456e8
Sarah Chieng <schieng@mit.edu>
2022-11-01 21:19:57 -0400

    deletes

--- client/components/Adjustfeed/AdjustfeedPage.vue
+++ client/components/Adjustfeed/AdjustfeedPage.vue
@@ -8,15 +8,12 @@
         <h2>Set Your Feed</h2>
       </header>
       <CreateAdjustfeedForm />
-      <!-- <toggle-button :value="true"
-                    :labels="{checked: 'Yes', unchecked: 'No'}"/> -->
           </section>
   </main>
 </template>
 
 <script>
 import CreateAdjustfeedForm from '@/components/Adjustfeed/CreateAdjustfeedForm.vue';
-// import { ToggleButton } from 'vue-js-toggle-button'
 
 export default {
   name: 'AdjustfeedPage',
